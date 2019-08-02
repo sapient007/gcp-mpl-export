8c4c52ba92e1a19e76fdbea67393d6a2a11cac9b
mingling <mingling@penguin>
2019-08-02 17:19:25 -0500

    added table name back

--- tasks/unnest.skus.sql
+++ tasks/unnest.skus.sql
@@ -1,1 +1,1 @@
-select category, sku.sku_id, ARRAY_TO_STRING(sku.prices, "|", "") as price,  sku.description from $BIGQUERY_TABLE, UNNEST(skus) as sku
+select category, sku.sku_id, ARRAY_TO_STRING(sku.prices, "|", "") as price,  sku.description from gcp_mpl.all_products, UNNEST(skus) as sku
