8be1d77d5ce703840e9ab9380b55abb95cfa9f64
Ming Ling <mingling@mingling-macbookpro.roam.corp.google.com>
2019-09-14 09:37:58 -0400

    updated remove dupe sql for a column name for unit

--- tasks/remove.duplicate.skus.sql
+++ tasks/remove.duplicate.skus.sql
@@ -1,1 +1,1 @@
-SELECT category, sku_id, usd, max(unit), description  FROM `mling-control-plane-demo.gcp_mpl.gsa_price_export` group by description, category, sku_id, usd order by category
+SELECT category, sku_id, usd, max(unit) as unit, description  FROM `mling-control-plane-demo.gcp_mpl.gsa_price_export` group by description, category, sku_id, usd order by category
